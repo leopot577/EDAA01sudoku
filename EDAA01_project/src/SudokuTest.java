4353f02ab3ae2fb30eb68eca84f761f914cc7960
David <david@natanael.lan>
2021-03-06 11:54:02 +0100

    Yeah baby

--- EDAA01_project/src/SudokuTest.java
+++ EDAA01_project/src/SudokuTest.java
@@ -1,25 +1,102 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SudokuTest {
-	private SudokuApplication a;
-	
-	@Before
+	private static final String IllegalArgumentException = null;
+	private SudokuBacktracker solver;
+
+	@BeforeEach
 	public void setUp() throws Exception {
-		a = new SudokuApplication();
+		solver = new SudokuBacktracker(9);
 	}
-	
-	@After
+
+	@AfterEach
 	public void tearDown() throws Exception {
-		a = null;
+		solver = null;
 	}
-	
+
 	@Test
 	void testEmptyGrid() {
+		assertTrue(solver.solve());
+	}
+
+	@Test
+	void testUnsolvableWrongInput() {
+		solver.setNumber(0, 0, 5);
+		solver.setNumber(8, 0, 5);
+		assertFalse(solver.solve());
+		solver.clear();
+		solver.setNumber(0, 0, 5);
+		solver.setNumber(0, 8, 5);
+		assertFalse(solver.solve());
+		solver.clear();
+		solver.setNumber(0, 0, 5);
+		solver.setNumber(1, 1, 5);
+		assertFalse(solver.solve());
+	}
+
+	@Test
+	void testUnsolvable() {
+		solver.setNumber(0, 0, 1);
+		solver.setNumber(1, 0, 2);
+		solver.setNumber(2, 0, 3);
+		solver.setNumber(0, 1, 4);
+		solver.setNumber(1, 1, 5);
+		solver.setNumber(2, 1, 6);
+		solver.setNumber(3, 2, 7);
+		assertFalse(solver.solve());
+		solver.setNumber(3, 2, 0);
+		assertTrue(solver.solve());
+	}
+
+	@Test
+	void testClear() {
+		solver.setNumber(0, 0, 5);
+		solver.setNumber(8, 0, 5);
+		assertFalse(solver.solve());
+		solver.clear();
+		assertTrue(solver.solve());
+	}
+
+	@Test
+	void testSolvable() {
+		int[][] startPos = { { 0, 0, 8, 0, 0, 9, 0, 6, 2 }, { 0, 0, 0, 0, 0, 0, 0, 0, 5 },
+				{ 1, 0, 2, 5, 0, 0, 0, 0, 0 }, { 0, 0, 0, 2, 1, 0, 0, 9, 0 }, { 0, 5, 0, 0, 0, 0, 6, 0, 0 },
+				{ 6, 0, 0, 0, 0, 0, 0, 2, 8 }, { 4, 1, 0, 6, 0, 8, 0, 0, 0 }, { 8, 6, 0, 0, 3, 0, 1, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 4, 0, 0 } };
+		solver.setMatrix(startPos);
+		assertTrue(solver.solve());
+	}
+
+	@Test
+	void testWrongInput() {
+		try {
+			solver.setNumber(4, 4, -1);
+			fail("Should show IllegalArgumentException");
+		} catch (Exception IllegalArgumentException) {
+		}
+
+		try {
+			solver.setNumber(4, 4, 10);
+			fail("Should show IllegalArgumentException");
+		} catch (Exception IllegalArgumentException) {
+		}
 		
+		// setting square to 0 equals resetting it, tested in application
+		//solver.setNumber(4, 4, 0);
+		//fail("Should show IllegalArgumentException");
+		
+		// can't test Sting input in Junit with suduko backtracking
+		// solver.setNumber(4, 4, "a");
+		// fail("Should show IllegalArgumentException");
+
 	}
 
 }
